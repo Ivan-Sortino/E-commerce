e16240b914fb8c596b005614667cf0985e13d310
IvanSortino <ivansortino2003@gmail.com>
2023-12-01 17:35:05 -0300

    navbar

--- react-project/src/components/Navbar/Navbar.jsx
+++ react-project/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import logo from "../../assets/LogoDonRouch.png"
+import cart from "../../assets/cart.png"
 import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <header className='navbar'>
-            <img src={logo} alt="" />
-            <div className='contentNav'>
-                <div className='contentSearch'>
-                    <div className='search'>
-                        <input type="text" placeholder='search' />
-                        <button><i class="bi bi-search"></i></button>
-                    </div>
-                    <div>
-                        <button><i class="bi bi-cart"></i></button>
-                    </div>
-                </div>
+        <header>
+            <div className='navbar'>
+                <img src={logo} alt="" />
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Don Rouch</a>
+                    <a href="#">Jewels</a>
+                    <a href="#">Custom</a>
+                    <a href="#">Mentions</a>
+                    <a href="#">Contact</a>
+                </nav>
             </div>
+
+            <div className='dos'>
+                <div>
+                    <i className="bi bi-search"></i>
+                    <input type="text" placeholder='Search' />
+                </div>
+                <div className='Cart'>
+                    <a href="">
+                        <img src={cart} alt="" />
+                        <span>0</span>
+                    </a>
+                </div>
+                
+            </div>
+
+
+
+
+
         </header>
     )
 }
